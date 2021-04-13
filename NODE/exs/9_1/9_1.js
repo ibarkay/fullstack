e9ec2cca3ed1db89c2a586478c1efdc05b228eb4
ibarkay <ibarkay@gmail.com>
2021-04-13 15:33:31 +0300

    exs express

--- NODE/exs/9_1/9_1.js
+++ NODE/exs/9_1/9_1.js
@@ -1,41 +1,71 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+const express = require("express");
+// const fs = require("fs");
+const path = require("path");
 const data = require("./users.json");
+const hbs = require("hbs");
+const app = express();
 
 const host = "localhost";
 const port = 8000;
-const index = fs.readFileSync("index.html").toString();
+const partPath = path.join(__dirname, "/temps");
+const viewPath = path.join(__dirname, "/views");
+const dir = path.join(__dirname);
 
-const requestListener = function (req, res) {
-	res.setHeader("Content-Type", "text/html");
-	switch (req.url) {
-		case "/":
-			res.writeHead(200);
-			var html = fs.readFileSync("./index.html");
-			res.write(html);
-			res.end();
-			break;
-		case "/raw-html":
-			res.writeHead(200);
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partPath);
 
-			res.end("<h1>Welcome</h1>");
-			break;
-		case "/index.css":
-			fs.readFile("index.css", function (err, data) {
-				res.writeHead(200, { "Content-Type": "text/css" });
-				res.write(data);
-				res.end();
-			});
-			break;
-		case "/users":
-			res.writeHead(200);
-			res.write(JSON.stringify(data));
-			res.end();
-			break;
-	}
-};
+app.use(express.static(dir));
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-	console.log(`Server is running on http://${host}:${port}`);
+app.get("", (req, res) => {
+	res.render("index");
 });
+
+app.get("/raw-html", (req, res) => {
+	res.render("raw");
+});
+
+app.get("/users", (req, res) => {
+	res.render("users", {
+		datas: JSON.stringify(data),
+	});
+});
+
+app.listen(3000, () => {
+	console.log("server start on 3000");
+});
+
+// const requestListener = function (req, res) {
+// 	res.setHeader("Content-Type", "text/html");
+// 	switch (req.url) {
+// 		case "/":
+// 			res.writeHead(200);
+// 			const html = fs.readFileSync("./index.html");
+// 			res.write(html);
+// 			res.end();
+// 			break;
+// 		case "/raw-html":
+// 			res.writeHead(200);
+
+// 			res.end("<h1>Welcome</h1>");
+// 			break;
+// 		case "/index.css":
+// 			fs.readFile("index.css", function (err, data) {
+// 				res.writeHead(200, { "Content-Type": "text/css" });
+// 				res.write(data);
+// 				res.end();
+// 			});
+// 			break;
+// 		case "/users":
+// 			res.writeHead(200);
+// 			res.write(JSON.stringify(data));
+// 			res.end();
+// 			break;
+// 	}
+// };
+
+// const server = http.createServer(requestListener);
+// server.listen(port, host, () => {
+// 	console.log(`Server is running on http://${host}:${port}`);
+// });
--- NODE/web-server/src/app.js
+++ NODE/web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+// Define path for express config
 const publicDir = path.join(__dirname, "../public");
-// app.com
-// app.com/about
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// setup handelbars engige and views loctions
 app.set("view engine", "hbs"); //COOL!
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static dir to serve
 app.use(express.static(publicDir));
 
 app.get("", (req, res) => {
@@ -23,9 +31,10 @@ app.get("/about", (req, res) => {
 	});
 });
 app.get("/help", (req, res) => {
-	res.render("about", {
+	res.render("help", {
 		title: "help",
-		name: "give you help",
+		helpText: "give you help",
+		name: "iftach",
 	});
 });
 
@@ -40,6 +49,22 @@ app.get("/weather", (req, res) => {
 			temp: "30c",
 		},
 	]);
+});
+
+app.get("/help/*", (req, res) => {
+	res.render("404", {
+		title: "404 help",
+		name: "iftach",
+		errMsg: "help aretc not found",
+	});
+});
+
+app.get("*", (req, res) => {
+	res.render("404", {
+		title: "error",
+		name: "iftach",
+		errMsg: "page not found",
+	});
 });
 
 app.listen(3000, () => {
